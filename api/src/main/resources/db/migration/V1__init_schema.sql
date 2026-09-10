98120e6eaed82843c5ec457a587c8c2892d74e62
Jason <chenjason214@gmail.com>
2026-09-10 00:31:44 -0400

    Fixed table in sql file

--- api/src/main/resources/db/migration/V1__init_schema.sql
+++ api/src/main/resources/db/migration/V1__init_schema.sql
@@ -1,8 +1,3 @@
-CREATE TABLE table_name (
-    column_name data_type constraints,
-    column_name data_type constraints
-);
-
 CREATE TABLE identities (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
