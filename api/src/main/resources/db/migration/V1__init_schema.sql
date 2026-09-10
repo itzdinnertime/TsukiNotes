8f4a28c9d18fec0792329011e19be6c654f3e141
Jason <chenjason214@gmail.com>
2026-09-10 00:23:04 -0400

    Added schema and rest of api folder contents

---
+++ api/src/main/resources/db/migration/V1__init_schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE table_name (
+    column_name data_type constraints,
+    column_name data_type constraints
+);
+
+CREATE TABLE identities (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
